127d5670dfc5f8b6eb839472430010c1eb0980b4
Amanda Gesiane <75225181+AmandaGesiane@users.noreply.github.com>
2023-07-02 08:26:21 -0300

    Versão final Raiphone

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  nome: { type: String, required: true },
+  valor: { type: String, required: true },
+  descricao: { type: String, required: false },
+}, {
+  timestamps: true,
+  collection: 'products',
+});
+
+
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
---
+++ frontend/raiphone-vue/src/components/admin/estoque/EstoqueComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+  <nav class="navbar navbar-expand-lg " id="navbar">
+    <div class="container">
+      <a class="navbar-brand" @click="$router.push('/admin')">RAIPHONE</a>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" @click="logOutUser">SAIR</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+    <!-- <div class="espacamento top">
+      <b-input type="search" variant="primary" placeholder="Nome do Usuario"></b-input>
+      <b-input type="search" variant="primary" placeholder="Cargo"></b-input>
+      <b-input type="search" variant="primary" placeholder="Liberar Modo Administrador? Sim/Não"></b-input>
+      <b-button block @click="$router.push('')" variant="primary">CADASTRAR</b-button>
+    </div> -->
+  </div>
+</template>
+
+<script src="./Estoque.js"></script>
+
+<style src="" scoped>
+#navbar {
+  background-color: #0C0074 !important;
+}
+</style>
